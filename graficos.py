2936d3b20a1646c2cc5de1a935dbbe77a4410e04
AgustinAltamirano <agustinga110704@gmail.com>
2024-11-24 21:04:05 -0300

    Agregada funcion para graficar carroceria respecto al terreno

--- graficos.py
+++ graficos.py
@@ -53,6 +53,28 @@ def comparar_valores_y(paso, b):
     plt.show()
 
 
+def comparar_valores_de_carroceria_con_terreno(paso, b):
+
+    valores_terreno = [funcion_de_terreno_con_loma_de_burro(i) for i in np.arange(0, 5, paso)]
+    _, y_values_burro = resolver(paso, b, False, funcion_de_terreno_con_loma_de_burro, derivada_de_funcion_de_terreno_con_loma_de_burro)
+    diferencia = [y_values_burro[i] - valores_terreno[i] for i in range(len(valores_terreno))]
+
+    t_values = [i * paso for i in range(len(valores_terreno))]
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(t_values, valores_terreno, label='Terreno con loma de burro')
+    plt.plot(t_values, y_values_burro, label='Carrocería')
+    plt.plot(t_values, diferencia, label='Diferencia entre ambos')
+    plt.title('Comparación de valores de y de la carrocería con el terreno')
+    plt.xlabel('Tiempo (s)')
+    plt.ylabel('Valor de y')
+    plt.legend()
+    plt.grid(True)
+    _, y_max = plt.ylim()
+    plt.yticks(np.arange(-0.10, y_max, 0.01))
+
+    plt.show()
+
 def comparar_tiempos(betas, paso, funcion_de_terreno, derivada_de_funcion_de_terreno):
     tiempos = []
 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 from operaciones import gauss_seidel_sin_amortiguamiento, estimar_orden_de_convergencia
-from graficos import graficar_orden_de_convergencia, graficar_valores_y, comparar_tiempos, comparar_valores_y
+from graficos import graficar_orden_de_convergencia, graficar_valores_y, comparar_tiempos, comparar_valores_y, comparar_valores_de_carroceria_con_terreno
 from funciones_auxiliares import funcion_de_terreno_constante, derivada_de_funcion_de_terreno_constante, funcion_de_terreno_con_loma_de_burro, derivada_de_funcion_de_terreno_con_loma_de_burro
 
 def main():
@@ -19,10 +19,13 @@ def main():
     #graficar_valores_y(betas, paso, funcion_de_terreno_constante, derivada_de_funcion_de_terreno_constante)
 
     # Comparar tiempos para conseguir beta de una serie de betas dadas, la funcion se le pasa como parametro
-    comparar_tiempos(betas, paso, funcion_de_terreno_constante, derivada_de_funcion_de_terreno_constante)
+    # comparar_tiempos(betas, paso, funcion_de_terreno_constante, derivada_de_funcion_de_terreno_constante)
 
     # Comparar valores de y entre terreno constante y loma de burro con beta = 0.5
-    #comparar_valores_y(paso, 0.5)
+    # comparar_valores_y(paso, 0.5)
+
+    # Comparar valores de carroceria con terreno
+    comparar_valores_de_carroceria_con_terreno(paso, 0.5)
 
 if __name__ == "__main__":
     main()
