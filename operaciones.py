5d23de519056e945a22a22e57307b403114f4ea3
AgustinAltamirano <agustinga110704@gmail.com>
2024-11-25 19:20:23 -0300

    Correccion del método de euler explícito

--- operaciones.py
+++ operaciones.py
@@ -24,15 +24,16 @@ def getLambda():
     return CONSTANTS['CONSTANTE_AMORTIGUAMIENTO']
 
 
-def gauss_seidel_sin_amortiguamiento(t_actual, y_actual, z_actual, paso, b, funcion_de_terreno,
+def gauss_seidel(t_actual, y_actual, z_actual, paso, b, funcion_de_terreno,
                                      derivada_de_funcion_de_terreno):
     c_actual = funcion_de_terreno(t_actual)
     c_siguiente = funcion_de_terreno(t_actual + paso)
-    semilla_y, semilla_z = siguiente_valor_de_euler_explicito_sin_amortiguamiento(c_actual, y_actual, z_actual, paso)
-    y_siguiente = semilla_y
-    z_siguiente = semilla_z
     c_siguiente_derivada = derivada_de_funcion_de_terreno(t_actual + paso)
     c_actual_derivada = derivada_de_funcion_de_terreno(t_actual)
+    semilla_y, semilla_z = siguiente_valor_de_euler_explicito(c_actual, c_actual_derivada, y_actual, z_actual, paso)
+    y_siguiente = semilla_y
+    z_siguiente = semilla_z
+    
 
     for _ in range(25):
         y_siguiente = y_actual + paso * ((1 - b) * z_actual + b * z_siguiente)
@@ -47,23 +48,22 @@ def gauss_seidel_sin_amortiguamiento(t_actual, y_actual, z_actual, paso, b, func
     return y_siguiente, z_siguiente
 
 
-def siguiente_valor_de_euler_explicito_sin_amortiguamiento(c, y_n, z_n, paso):
+def siguiente_valor_de_euler_explicito(c, c_derivada, y_n, z_n, paso):
     y_siguiente = y_n + paso * z_n
-    z_siguiente = z_n + paso * CONSTANTS['CONSTANTE_ELASTICA'] / MASA * (c - y_n)
+    z_siguiente = z_n + paso / MASA * ( CONSTANTS['CONSTANTE_ELASTICA'] * (c - y_n) + CONSTANTS['CONSTANTE_AMORTIGUAMIENTO'] * (c_derivada - z_n)) 
     return y_siguiente, z_siguiente
-
 
 def resolver(paso, b, imprimir_valores_intermedios, funcion_de_terreno, derivada_de_funcion_de_terreno):
     y_actual = Y_INICIAL
     z_actual = Z_INICIAL
-    y_values = []
+    y_values = [y_actual]
 
     if imprimir_valores_intermedios:
         print(f"Avance número {0}, t = {0}, y = {y_actual}")
 
     for avance_actual in range(1, int(TIEMPO_FINAL / paso) + 1):
         t_actual = avance_actual * paso
-        y_actual, z_actual = gauss_seidel_sin_amortiguamiento(t_actual, y_actual, z_actual, paso, b, funcion_de_terreno,
+        y_actual, z_actual = gauss_seidel(t_actual, y_actual, z_actual, paso, b, funcion_de_terreno,
                                                               derivada_de_funcion_de_terreno)
         y_values.append(y_actual)
         if imprimir_valores_intermedios:
